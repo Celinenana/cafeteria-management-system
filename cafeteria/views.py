c4ed3ea69ce928aa66f322defd5e776bc650ed93
Celine Zhou <92128819+Celinenana@users.noreply.github.com>
2024-11-30 22:23:42 -0500

    Add overview page

--- cafeteria/views.py
+++ cafeteria/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from .models import Menu
 
-# Create your views here.
+def home(request):
+    menu_items = Menu.objects.all()  # 从数据库中获取菜单数据
+    return render(request, 'home.html', {'menu_items': menu_items})
+
+
